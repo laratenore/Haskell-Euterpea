061f8fb3b8eb902ac23f2f863921e4f3fcb0b6cb
Ramon <ramon-neres@hotmail.com>
2018-08-07 18:10:34 -0300

    Melhorando funções

--- Exemplos/midiTest.hs
+++ Exemplos/midiTest.hs
@@ -1,29 +1,12 @@
 import Euterpea
-import Data.Either
 import Codec.Midi
-import Euterpea.IO.MIDI.FromMidi2
+                   
+playFromMidi :: IO (Either String Midi) -> IO ()
+playFromMidi a = a >>= playMidi'
+    where
+        playMidi' :: (Either String Midi) -> IO ()
+        playMidi' (Right a) = play $ fromMidi a
+        playMidi' (Left a)  = error a
 
-
-track0 = [(0,  NoteOn 0 60 80),
-          (24, NoteOff 0 60 0),
-          (0,  TrackEnd)]
-          
-track1 = [(0,  NoteOn 0 64 80),
-          (24, NoteOn 0 64 0),
-          (0,  TrackEnd)]
-          
-myMidi = Midi { fileType = MultiTrack, 
-                timeDiv  = TicksPerBeat 24, 
-                tracks   = [track0, track1] }
-
-{-fileToMusic :: [Char] -> IO Music1
-fileToMusic cs = do a <- importFile cs
-                    b <- (fromRight myMidi) <$> a
-                    c <- pure fromMidi <*> b
-                    c-}
-                    
-          
-midiFileIOEither = importFile "midis/twinkletwinkle.mid"
-midiFileIO       = (fromRight myMidi) <$> midiFileIOEither
-musicIO          = pure fromMidi <*> midiFileIO
-song             = musicIO >>= play
+playFromPath :: String -> IO ()
+playFromPath cs = playFromMidi $ importFile cs
