56d7104cf8b5b5cef718758aafaeec8c92d6cd3b
Ramon <ramon-neres@hotmail.com>
2018-08-08 21:21:37 -0300

    Biblioteca

--- Exemplos/midiTest.hs
+++ Exemplos/midiTest.hs
@@ -5,8 +5,11 @@ playFromMidi :: IO (Either String Midi) -> IO ()
 playFromMidi a = a >>= playMidi'
     where
         playMidi' :: (Either String Midi) -> IO ()
-        playMidi' (Right a) = play $ fromMidi a
+        playMidi' (Right a) = play $ invert (fromMidi a)
         playMidi' (Left a)  = error a
 
-playFromPath :: String -> IO ()
-playFromPath cs = playFromMidi $ importFile cs
+playFromPath :: IO ()
+playFromPath = do putStr "Digite o diretorio do arquivo: " 
+                  cs <- getLine
+                  playFromMidi $ importFile cs
+                  
