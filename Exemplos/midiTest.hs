a779c49cdc8b2fe9801dd797a9fd9245db7f91dc
Ramon <ramon-neres@hotmail.com>
2018-08-08 23:21:45 -0300

    Correção

--- Exemplos/midiTest.hs
+++ Exemplos/midiTest.hs
@@ -5,7 +5,7 @@ playFromMidi :: IO (Either String Midi) -> IO ()
 playFromMidi a = a >>= playMidi'
     where
         playMidi' :: (Either String Midi) -> IO ()
-        playMidi' (Right a) = play $ invert (fromMidi a)
+        playMidi' (Right a) = play $ fromMidi a
         playMidi' (Left a)  = error a
 
 playFromPath :: IO ()
